212ae3e5542d971aacfd63546bf4fc287a374e75
Katsyaryna <katsyarynnks@gmail.com>
2024-11-04 22:33:51 +0300

    context foe c

---
+++ LabWork4_PartC/Context_for_C.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void menuc();
+
+int main()
+{
+    menuc();
+    int exit=0;
+    while (exit!=1) {
+    char *s=NULL;
+    size_t ssize = 0;
+    __ssize_t stringsize;
+    const char* valid_chars = "12345";
+    int x;
+   
+    do
+    {
+        
+        printf("\nВведите номер желаемой операции: ");
+        stringsize=getline(&s, &ssize, stdin);
+        if(stringsize==-1){
+            perror("Ошибка ввода");
+            return 1;
+        }
+        x=atoi(s);
+       // free(s);   
+    }while ( x < 1 || x > 5);
+
+    switch (x)
+        {
+        case 1:
+            printf("описание задания");
+            break;
+
+        case 2:
+            printf("Буткевич Екатерина, группа 453501, вариант  7");
+            break;
+
+        case 3:
+            
+            break;
+
+        case 4:
+             menuc();
+            break;
+
+        case 5:
+        exit= 1;
+        break;
+        }
+    }
+    
+    return 0;
+}
+
+void menuc()
+{
+    printf("\nСуть программы - 1\nКто выполнил задание? - 2\nЗапуск программы - 3\nМеню - 4\nЗавершить программу - 5\n");
+}
--- LabWork4_PartC/Task_4.c
+++ LabWork4_PartC/Task_4.c
@@ -1,6 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+void menuc();
+int task4();
+
 int main()
+{
+    menuc();
+    int exit = 0;
+    while (exit != 1)
+    {
+        char *s = NULL;
+        size_t ssize = 0;
+        __ssize_t stringsize;
+        const char *valid_chars = "12345";
+        int x;
+
+        do
+        {
+
+            printf("\nВведите номер желаемой операции: ");
+            stringsize = getline(&s, &ssize, stdin);
+            if (stringsize == -1)
+            {
+                perror("Ошибка ввода");
+                return 1;
+            }
+            x = atoi(s);
+            // free(s);
+        } while (x < 1 || x > 5);
+
+        switch (x)
+        {
+        case 1:
+            printf("Найти диагональ в 3хмерном массиве с найбольшей суммой элементов\n");
+            break;
+
+        case 2:
+            printf("Буткевич Екатерина, группа 453501, вариант  7\n");
+            break;
+
+        case 3:
+            task4();
+            getline(&s, &ssize, stdin);
+            break;
+
+        case 4:
+            menuc();
+            break;
+
+        case 5:
+            exit = 1;
+            break;
+        }
+    }
+    return 0;
+}
+
+void menuc()
+{
+    printf("\nСуть программы - 1\nКто выполнил задание? - 2\nЗапуск программы - 3\nМеню - 4\nЗавершить программу - 5\n");
+}
+
+int task4()
 {
     int n;
     printf("Введите размер массива: ");
@@ -76,6 +138,6 @@ int main()
         printf("\nСумма элементов диагонали из левого заднего верхнего угла максимальная");
     else
         printf("\nЕсть равные значения сумм элементов диагоналей");
-    
+
     return 0;
 }
